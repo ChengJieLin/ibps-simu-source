38e0fbd9e98c53989f8fc6268295074939c43a2e
linchengjie <508228839@qq.com>
2024-04-18 16:58:29 +0800

    后台服务设置启动端口48080

--- ibps-simu/ibps-simu-server/src/main/java/com/ibeyoung/simu/SimuServerApp.java
+++ ibps-simu/ibps-simu-server/src/main/java/com/ibeyoung/simu/SimuServerApp.java
@@ -1,7 +1,12 @@
 package com.ibeyoung.simu;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class SimuServerApp {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(SimuServerApp.class, args);
     }
 }
