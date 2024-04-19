70fdcd03b53795b06c73f1af4c74d0403dc827e6
linchengjie <linchengjie.x@gmail.com>
2024-04-20 00:02:05 +0800

    增加mybatis模块

---
+++ ibps-simu/framework/framework-mybatis-plus/src/main/java/com/ibeyoung/framework/mybatisplus/FrameworkMybatisAutoConfiguration.java
@@ -0,0 +1,16 @@
+package com.ibeyoung.framework.mybatisplus;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@AutoConfiguration
+public class FrameworkMybatisAutoConfiguration {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor()); // 分页插件
+        return mybatisPlusInterceptor;
+    }
+}
